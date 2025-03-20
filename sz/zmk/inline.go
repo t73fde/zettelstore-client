f6e9377dde0378ffb252b0e309719894de3e119b
stern <ds@zettelstore.de>
2025-03-20 15:13:31 +0000

    Got zsc/input/ScanEntity
    FossilOrigin-Name: 95f53cd9aa0870ab3c3405e8e311591e09a5de47fd873faecc606be8958f15cb

--- sz/zmk/inline.go
+++ sz/zmk/inline.go
@@ -19,6 +19,7 @@ import (
 	"strings"
 
 	"t73f.de/r/sx"
+	"t73f.de/r/zsc/sz"
 	"t73f.de/r/zsx"
 	"t73f.de/r/zsx/input"
 )
@@ -464,7 +465,7 @@ func parseNdash(inp *input.Input) (*sx.Pair, bool) {
 }
 
 func parseEntity(inp *input.Input) (*sx.Pair, bool) {
-	if text, ok := inp.ScanEntity(); ok {
+	if text, ok := sz.ScanEntity(inp); ok {
 		return zsx.MakeText(text), true
 	}
 	return nil, false
