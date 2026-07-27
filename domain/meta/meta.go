8a116218ffc4f2419d0ed1cfd881ae01ed3eb1ae
stern <ds@zettelstore.de>
2026-07-27 13:04:51 +0000

    fix(domain) KeyBoxName is of type "word"
    FossilOrigin-Name: 1937fb5de980de0ce7390a4e8728925bb9e3b6159ba0d35c0d81161627b53fb1

--- domain/meta/meta.go
+++ domain/meta/meta.go
@@ -182,7 +182,7 @@ func init() {
 	registerKey(KeyAuthor, TypeString, usageUser, "")
 	registerKey(KeyBack, TypeIDSet, usageProperty, "")
 	registerKey(KeyBackward, TypeIDSet, usageProperty, "")
-	registerKey(KeyBoxName, TypeString, usageProperty, "")
+	registerKey(KeyBoxName, TypeWord, usageProperty, "")
 	registerKey(KeyBoxNumber, TypeNumber, usageProperty, "")
 	registerKey(KeyCopyright, TypeString, usageUser, "")
 	registerKey(KeyCreated, TypeTimestamp, usageComputed, "")
